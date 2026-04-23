d076b16e8f911cc8ce982069db41aea08271c871
amir <amirabrashid@yahoo.com>
2026-04-24 01:14:58 +0800

    indent

--- lua/pack.lua
+++ lua/pack.lua
@@ -1,8 +1,8 @@
 vim.pack.add({
-    -- mini
+	-- mini
 	{ src = "https://github.com/echasnovski/mini.pick" },
-    { src = "https://github.com/echasnovski/mini.icons" },
-    { src = "https://github.com/echasnovski/mini.completion" },
+	{ src = "https://github.com/echasnovski/mini.icons" },
+	{ src = "https://github.com/echasnovski/mini.completion" },
 
 	{ src = "https://github.com/mbbill/undotree" },
 
@@ -16,11 +16,10 @@ vim.pack.add({
 	{ src = "https://github.com/kevinhwang91/nvim-ufo" }, --enabled
 	{ src = "https://github.com/kevinhwang91/promise-async" }, --nvim-ufo dependency
 
-    -- 	oil
+	-- 	oil
 	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/nvim-tree/nvim-web-devicons" }, --optional dependency
-
 })
 
-require('mason').setup()
-require('mason-lspconfig').setup()
+require("mason").setup()
+require("mason-lspconfig").setup()
